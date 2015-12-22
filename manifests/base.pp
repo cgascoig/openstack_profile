17412ecdd87682a946f82c55062fc46046c6f0f3
Chris Gascoigne <cgascoig@cisco.com>
2015-12-23 09:34:22 +1100

    add cloud-archive for liberty

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,5 +1,11 @@
 class profile::base {
   class { 'ntp':
-    servers    => ["ntp.esl.cisco.com"]
+    servers    => ["ntp.esl.cisco.com"],
   }
+  
+  class { 'apt':
+    apt_update_frequency   => "daily",
+    ppas                   => ["cloud-archive:liberty"],
+  }
+  
 }
