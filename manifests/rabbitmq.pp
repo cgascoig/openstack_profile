feb8d8704d984d0baefb50ee2e224ca31ff66c12
Chris Gascoigne <cgascoig@cisco.com>
2015-12-23 10:19:08 +1100

    add rabbitmq

---
+++ manifests/rabbitmq.pp
@@ -0,0 +1,21 @@
+class profile::rabbitmq (
+  String username = 'openstack',
+  String password = 'C1sco123',
+) {
+  class { '::rabbitmq' : }
+  
+  rabbitmq_user { $username:
+    admin    => false,
+    password => $password,
+  }
+  
+  rabbitmq_vhost { '/':
+    ensure  => present
+  }
+  
+  rabbitmq_user_permissions { 'openstack@/':
+    configure_permission   => '.*',
+    read_permission        => '.*',
+    write_permission       => '.*',
+  }
+}
