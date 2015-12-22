b6495c4fb3aa5821e5fea89179c0eda6757838af
Chris Gascoigne <cgascoig@cisco.com>
2015-12-23 09:54:17 +1100

    add user cgascoig

--- manifests/base.pp
+++ manifests/base.pp
@@ -5,4 +5,23 @@ class profile::base {
   class {'apt': }
   apt::ppa { 'cloud-archive:liberty': }
   
+  group {'cgascoig':
+    ensure  => present,
+  }
+  
+  user {'cgascoig':
+    ensure   => present,
+    shell    => '/bin/bash',
+    groups   => 'sudo',
+    gid      => 'cgascoig',
+  }
+  
+  file {'/home/cgascoig':
+    ensure    => directory,
+    owner     => "cgascoig",
+    group     => "cgascoig",
+    mode      => '0750',
+    require   => [ User['cgascoig'], Group['cgascoig'] ]
+  }
+  
 }
