b6f0bb1b40613cd0c10827c24557f030862cb143
Chris Gascoigne <cgascoig@aci-linux-1.domain.name>
2015-12-31 16:57:46 +1100

    disable default vhost in apache

--- manifests/horizon.pp
+++ manifests/horizon.pp
@@ -6,6 +6,10 @@ class profile::horizon (
     
   }
 
+  class { 'apache':
+    default_vhost    => false,
+  }
+
   class { 'horizon':
     secret_key      => '12345',
     cache_server_ip => '127.0.0.1',
