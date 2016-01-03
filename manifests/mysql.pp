8107c8a8c3480c2b870bf6ccf06d565f5a696e15
Chris Gascoigne <cgascoig@cisco.com>
2016-01-03 19:40:36 +1100

    set mysql max_connections to 1000

--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -2,6 +2,11 @@ class profile::mysql (
   String $root_password = 'C1sco123',
 ) {
   class { 'mysql::server': 
-    root_password   => $root_password,
+    root_password     => $root_password,
+    override_options  => {
+      'mysqld'  => {
+        'max_connections' => '1000',
+      }
+    }
   }
 }
