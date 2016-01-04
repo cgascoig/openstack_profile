98b5813600051a096e7ceef8ec22ff62ee9f6ae9
Chris Gascoigne <cgascoig@cisco.com>
2016-01-04 15:57:47 +1100

    initial support for centos

--- manifests/base.pp
+++ manifests/base.pp
@@ -2,8 +2,19 @@ class profile::base {
   class { 'ntp':
     servers    => ["ntp.esl.cisco.com"],
   }
-  class {'apt': }
-  apt::ppa { 'cloud-archive:liberty': }
+  
+  case $::osfamily {
+    'Debian': {
+      class {'apt': }
+      apt::ppa { 'cloud-archive:liberty': }
+    }
+    'RedHat': {
+      package {'centos-release-openstack-liberty':
+        ensure    => present,
+      }
+    }
+  }
+  
   
   group {'cgascoig':
     ensure  => present,
